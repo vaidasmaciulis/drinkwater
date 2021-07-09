198c4e0db89770b587d5274a25fb8c7f84217a65
DESKTOP-739BTMQ\Vaidas <vaidas.maciulis@gmail.com>
2021-07-10 01:35:44 +0300

    Added water stat

---
+++ Data/Scripts/DrinkWater/Server.cs
@@ -0,0 +1,63 @@
+﻿using Sandbox.Game.Components;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using System.Collections.Generic;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Interfaces;
+using VRage.ModAPI;
+using VRage.Utils;
+
+namespace DrinkWater
+{
+	[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+	public class Server : MySessionComponentBase
+	{
+		private const int SKIP_TICKS = 300;
+		private const float WATER_USAGE = 0.5f;
+		private const float WATER_DAMAGE = 0.1f;
+		
+		private static List<IMyPlayer> players = new List<IMyPlayer>();
+		private static int skipTick = 0;
+
+		public override void UpdateAfterSimulation()
+        {
+            //base.UpdateAfterSimulation();
+
+			if (skipTick++ <= SKIP_TICKS)
+            {
+				return;
+            } 
+			else
+            {
+				skipTick = 0;
+			}
+
+			players.Clear();
+			MyAPIGateway.Players.GetPlayers(players);
+
+			foreach (IMyPlayer player in players)
+            {
+				IMyEntity entity = player.Controller.ControlledEntity.Entity;
+
+				MyEntityStatComponent statComp;
+				entity.Components.TryGet(out statComp);
+				MyEntityStat water;
+				statComp.TryGetStat(MyStringHash.GetOrCompute("Water"), out water);
+
+				if (water.Value > 0)
+				{
+					water.Decrease(WATER_USAGE, null);
+					MyAPIGateway.Utilities.ShowMessage("DEBUG", "Water: " + water.Value);
+				}
+
+				if (water.Value <= 0)
+				{
+					var destroyable = entity as IMyDestroyableObject;
+					destroyable.DoDamage(WATER_DAMAGE, MyStringHash.GetOrCompute("Unknown"), true);
+				}
+			}
+
+		}
+    }
+}
