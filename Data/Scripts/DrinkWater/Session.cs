111b460abb3b686523dab2ea4526dece3874f357
DESKTOP-739BTMQ\Vaidas <vaidas.maciulis@gmail.com>
2021-07-31 15:06:41 +0300

    Debug change restored

--- Data/Scripts/DrinkWater/Session.cs
+++ Data/Scripts/DrinkWater/Session.cs
@@ -18,7 +18,7 @@ namespace DrinkWater
         private const float WATER_DAMAGE = 0.5f;
         private const float FOOD_USAGE = 0.03f;
         private const float FOOD_DAMAGE = 0.3f;
-        private const float SLEEP_USAGE = 1.02f;
+        private const float SLEEP_USAGE = 0.02f;
         private const float SLEEP_DAMAGE = 0.3f;
         private const float SLEEP_GAIN_SITTING = 2f;
         private const float SLEEP_GAIN_SLEEPING = 100f;
