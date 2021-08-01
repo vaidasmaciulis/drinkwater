36fe38f976a94c60c47737be5873b4369fbc6598
DESKTOP-739BTMQ\Vaidas <vaidas.maciulis@gmail.com>
2021-08-01 16:27:51 +0300

    No food/water usage while in cryo or bed. Damage from runnning out of stats increased

--- Data/Scripts/DrinkWater/Session.cs
+++ Data/Scripts/DrinkWater/Session.cs
@@ -15,11 +15,11 @@ namespace DrinkWater
     {
         private const int TICKS_TO_SKIP = 300;
         private const float WATER_USAGE = 0.1f;
-        private const float WATER_DAMAGE = 0.5f;
+        private const float WATER_DAMAGE = 5f;
         private const float FOOD_USAGE = 0.03f;
-        private const float FOOD_DAMAGE = 0.3f;
+        private const float FOOD_DAMAGE = 3f;
         private const float SLEEP_USAGE = 0.02f;
-        private const float SLEEP_DAMAGE = 0.3f;
+        private const float SLEEP_DAMAGE = 2f;
         private const float SLEEP_GAIN_SITTING = 5f;
         private const float SLEEP_GAIN_SLEEPING = 100f;
 
@@ -76,37 +76,40 @@ namespace DrinkWater
                     }
                 };
 
-                if (water.Value > 0)
-                {
-                    water.Decrease(WATER_USAGE, null);
-                } 
-                else
+                if (water.Value <= 0)
                 {
                     player.Character.DoDamage(WATER_DAMAGE, MyStringHash.GetOrCompute("Unknown"), true);
                 }
 
-                if (food.Value > 0)
-                {
-                    food.Decrease(FOOD_USAGE, null);
-                } 
-                else
+                if (food.Value <= 0)
                 {
                     player.Character.DoDamage(FOOD_DAMAGE, MyStringHash.GetOrCompute("Unknown"), true);
                 }
 
-                if (player.Character.CurrentMovementState == MyCharacterMovementEnum.Sitting)
-				{
-                    float sleepGain = player.Controller.ControlledEntity.ToString().StartsWith("MyCryoChamber") ? SLEEP_GAIN_SLEEPING : SLEEP_GAIN_SITTING;
-                    sleep.Increase(sleepGain, null);
-				}
-                else if (sleep.Value > 0)
-                {
-                    sleep.Decrease(SLEEP_USAGE, null);
-                }
-                else
+                if (sleep.Value <= 0)
                 {
                     player.Character.DoDamage(SLEEP_DAMAGE, MyStringHash.GetOrCompute("Unknown"), true);
                 }
+
+                bool inCryoOrBed = false;
+
+                if (player.Character.CurrentMovementState == MyCharacterMovementEnum.Sitting)
+				{
+                    inCryoOrBed = player.Controller.ControlledEntity.ToString().StartsWith("MyCryoChamber");
+                    float sleepGain = inCryoOrBed ? SLEEP_GAIN_SLEEPING : SLEEP_GAIN_SITTING;
+                    sleep.Increase(sleepGain, null);
+				} 
+                else
+				{
+                    sleep.Decrease(SLEEP_USAGE, null);
+                }
+
+                if (!inCryoOrBed)
+				{
+                    food.Decrease(FOOD_USAGE, null);
+                    water.Decrease(WATER_USAGE, null);
+                }
+
             }
 
         }
