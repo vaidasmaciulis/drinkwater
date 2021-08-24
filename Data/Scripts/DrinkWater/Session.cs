cc3f72bd9759e7164fddce56cbc997aeaba99917
DESKTOP-739BTMQ\Vaidas <vaidas.maciulis@gmail.com>
2021-08-24 16:54:32 +0300

    Moved stats logic to client side

--- Data/Scripts/DrinkWater/Session.cs
+++ Data/Scripts/DrinkWater/Session.cs
@@ -1,7 +1,6 @@
 ﻿using Sandbox.Game.Components;
 using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
-using System.Collections.Generic;
 using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.ModAPI;
@@ -12,93 +11,125 @@ namespace DrinkWater
 	[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
 	public class Session : MySessionComponentBase
 	{
-		private const int TICKS_TO_SKIP = 300;
-		private const float WATER_USAGE = 0.1f;
-		private const float WATER_DAMAGE = 5f;
-		private const float FOOD_USAGE = 0.05f;
-		private const float FOOD_DAMAGE = 3f;
-		private const float SLEEP_USAGE = 0.03f;
-		private const float SLEEP_DAMAGE = 2f;
-		private const float SLEEP_GAIN_SITTING = 3f;
+		private static int skippedTicks = 0;
+		private IMyPlayer player;
+		private MyEntityStatComponent statComp;
+		private MyEntityStat water;
+		private MyEntityStat food;
+		private MyEntityStat sleep;
+		private bool isDedicated;
+		private const float WATER_USAGE = 0.03f;
+		private const float WATER_DAMAGE = 0.015f;
+		private const float FOOD_USAGE = 0.015f;
+		private const float FOOD_DAMAGE = 1f;
+		private const float SLEEP_USAGE = 0.01f;
+		private const float SLEEP_DAMAGE = 0.75f;
+		private const float SLEEP_GAIN_SITTING = 1f;
 		private const float SLEEP_GAIN_SLEEPING = 100f;
 
-		private static List<IMyPlayer> players = new List<IMyPlayer>();
-		private static int skippedTicks = 0;
-
-		public override void UpdateAfterSimulation()
+		public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
 		{
-			if (skippedTicks++ < TICKS_TO_SKIP)
+			isDedicated = MyAPIGateway.Utilities.IsDedicated;
+			if (isDedicated)
 			{
 				return;
 			}
-			skippedTicks = 0;
-
-			players.Clear();
-			MyAPIGateway.Players.GetPlayers(players);
-
-			foreach (IMyPlayer player in players)
-			{
-				if (player.IsBot == true)
-				{
-					continue;
-				}
-
-				MyEntityStatComponent statComp = player.Character?.Components?.Get<MyEntityStatComponent>();
-
-				if (statComp == null)
-				{
-					continue;
-				}
-
-				MyEntityStat water = GetPlayerStat(statComp, "Water");
-				MyEntityStat food = GetPlayerStat(statComp, "Food");
-				MyEntityStat sleep = GetPlayerStat(statComp, "Sleep");
-
-				if (water.Value <= 0)
-				{
-					player.Character.DoDamage(WATER_DAMAGE, MyDamageType.Unknown, true);
-				}
-
-				if (food.Value <= 0)
-				{
-					player.Character.DoDamage(FOOD_DAMAGE, MyDamageType.Unknown, true);
-				}
-
-				if (sleep.Value <= 0)
-				{
-					player.Character.DoDamage(SLEEP_DAMAGE, MyDamageType.Unknown, true);
-				}
-
-				bool inCryoOrBed = false;
-
-				if (player.Character.CurrentMovementState == MyCharacterMovementEnum.Sitting &&
-					(player.Controller.ControlledEntity as IMyShipController) != null &&
-					!(player.Controller.ControlledEntity as IMyShipController).CanControlShip)
-				{
-					//Sitting, but not working
-					inCryoOrBed = player.Controller.ControlledEntity.ToString().StartsWith("MyCryoChamber");
-					float sleepGain = inCryoOrBed ? SLEEP_GAIN_SLEEPING : SLEEP_GAIN_SITTING;
-					sleep.Increase(sleepGain, null);
-				}
-				else
-				{
-					sleep.Decrease(SLEEP_USAGE, null);
-				}
-
-				if (!inCryoOrBed)
-				{
-					food.Decrease(FOOD_USAGE, null);
-					water.Decrease(WATER_USAGE, null);
-				}
-			}
-
+			UpdateAfterSimulation100();
 		}
 
-		private MyEntityStat GetPlayerStat(MyEntityStatComponent statComp, string statName)
+		public override void UpdateAfterSimulation()
 		{
-			MyEntityStat stat;
-			statComp.TryGetStat(MyStringHash.GetOrCompute(statName), out stat);
-			return stat;
+			if (isDedicated)
+			{
+				return;
+			}
+			if (skippedTicks++ <= 100)
+			{
+				if (skippedTicks % 10 == 0)
+				{
+					UpdateAfterSimulation10();
+				}
+			}
+			else
+			{
+				skippedTicks = 0;
+				UpdateAfterSimulation100();
+			}
+		}
+
+		public void UpdateAfterSimulation10()
+		{
+			if (player?.Character == null || statComp == null || water == null || food == null || sleep == null)
+			{
+				return;
+			}
+			if (player.Character.EnabledHelmet)
+			{
+				if (water.StatRegenLeft > 0)
+				{
+					player.Character.SwitchHelmet();
+					MyAPIGateway.Utilities.ShowNotification("Helmet opened to drink!", 3000);
+				}
+				else if (food.StatRegenLeft > 0)
+				{
+					player.Character.SwitchHelmet();
+					MyAPIGateway.Utilities.ShowNotification("Helmet opened to eat!", 3000);
+				}
+			}
+		}
+
+		public void UpdateAfterSimulation100()
+		{
+			player = MyAPIGateway.Session?.Player;
+			statComp = player?.Character?.Components?.Get<MyEntityStatComponent>();
+			if (statComp == null)
+			{
+				return;
+			}
+			statComp.TryGetStat(MyStringHash.GetOrCompute("Water"), out water);
+			statComp.TryGetStat(MyStringHash.GetOrCompute("Food"), out food);
+			statComp.TryGetStat(MyStringHash.GetOrCompute("Sleep"), out sleep);
+			if (water == null || food == null || sleep == null)
+			{
+				return;
+			}
+
+			if (water.Value <= 0)
+			{
+				player.Character.DoDamage(WATER_DAMAGE, MyDamageType.Unknown, true);
+			}
+
+			if (food.Value <= 0)
+			{
+				player.Character.DoDamage(FOOD_DAMAGE, MyDamageType.Unknown, true);
+			}
+
+			if (sleep.Value <= 0)
+			{
+				player.Character.DoDamage(SLEEP_DAMAGE, MyDamageType.Unknown, true);
+			}
+
+			bool inCryoOrBed = false;
+
+			if (player.Character.CurrentMovementState == MyCharacterMovementEnum.Sitting &&
+				(player.Controller.ControlledEntity as IMyShipController) != null &&
+				!(player.Controller.ControlledEntity as IMyShipController).CanControlShip)
+			{
+				//Sitting, but not working
+				inCryoOrBed = player.Controller.ControlledEntity.ToString().StartsWith("MyCryoChamber");
+				float sleepGain = inCryoOrBed ? SLEEP_GAIN_SLEEPING : SLEEP_GAIN_SITTING;
+				sleep.Increase(sleepGain, null);
+			}
+			else
+			{
+				sleep.Decrease(SLEEP_USAGE, null);
+			}
+
+			if (!inCryoOrBed)
+			{
+				food.Decrease(FOOD_USAGE, null);
+				water.Decrease(WATER_USAGE, null);
+			}
 		}
 	}
 }
